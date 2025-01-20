4b71d7f021f15391346704fdc42023ba86a6e8dc
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-01-20 20:17:47 +0600

    completed the contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer></ToastContainer>
       <Header></Header>
       <About></About>
       <Projects></Projects>
       <Testimonials></Testimonials>
+      <Contact></Contact>
     </div>
   );
 };
