7f7add757898179f94d459d08869a5a6c3c0e640
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-01-20 18:34:21 +0600

    completed the projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import { assets, projectsData } from "../assets/assets";
+import { useEffect } from "react";
 const Projects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  useEffect(() => {
+    const updateCardsToShow = () => {
+      if (window.innerWidth >= 1024) {
+        setCardsToShow(projectsData.length);
+      } else {
+        setCardsToShow(1);
+      }
+    };
+    updateCardsToShow();
+
+    window.addEventListener("resize", updateCardsToShow);
+    return () => window.removeEventListener("resize", updateCardsToShow);
+  }, []);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div
       className="container mx-auto px-6 py-4 pt-20 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -14,6 +44,54 @@ const Projects = () => {
       <p className="text-gray-500 max-w-80 text-center mb-8 mx-auto">
         Crafting Spaces, Building Legacies-Explore Our Portfolio
       </p>
+
+      {/* slider-buttons */}
+      <div className="flex justify-end items-center mb-8">
+        <button
+          onClick={prevProject}
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Previous Project"
+        >
+          <img src={assets.left_arrow} alt="previous" />
+        </button>
+        <button
+          onClick={nextProject}
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Next Project"
+        >
+          <img src={assets.right_arrow} alt="next" />
+        </button>
+      </div>
+
+      {/* project-slider-container */}
+      <div className="overflow-hidden">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`,
+          }}
+        >
+          {projectsData.map((project, index) => (
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                className="w-full h-auto mb-14"
+                src={project.image}
+                alt={project.title}
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span>|</span> {project.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
